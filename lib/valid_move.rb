4c58ddf9eed57a190b20bd992aba9869a1796747
Bryan McAfee <brymcafee@gmail.com>
2017-10-31 16:38:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  if position.to_i.between?(1, 9) && postion_taken?(board, position.to_i - 1)
+  move = position.to_i - 1
+  if move.to_i.between?(0, 8) && postion_taken?(board, move)
   true
 else
   false
