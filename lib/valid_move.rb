bf98822d06e028f6eb26bb6a9450eaafcd477acc
Bryan McAfee <brymcafee@gmail.com>
2017-10-31 15:19:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,6 +6,6 @@ def valid_move?(board, position)
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
-  return false if [" ", ""].include?(board[test])
-  return true if ["X", "O"].include?(board[test])
+  return false if [" ", ""].include?(board[position])
+  return true if ["X", "O"].include?(board[position])
 end
