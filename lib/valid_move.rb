a62770cc383770dc592ec5630bed5d2b03e18c5c
Bryan McAfee <brymcafee@gmail.com>
2017-10-31 16:14:46 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ end
 def position_taken?(board, position)
   if board[position] == "X" || board[position] == "O"
     true
-  elsif board[position] == " " || "" || nil
+  elsif board[position] == " " || board[position] == "" || board[position] == nil
     false
   else
     false
