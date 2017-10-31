f6e9a149c480c0f3a34df116754636892e92ad42
Bryan McAfee <brymcafee@gmail.com>
2017-10-31 15:59:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,11 +11,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
-  if board[position] == " "
-    false
-  elsif board[position] == ""
-    false
-  elsif board[position] == nil
+  if board[position] == ” ” || board[position] == “” || board[position] == nil
     false
   else board[position] == "X" || board[position] == "O"
     true
