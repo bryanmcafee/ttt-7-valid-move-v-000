69bef20f107fde69d44979d23c9b732901257dea
Bryan McAfee <brymcafee@gmail.com>
2017-10-31 15:21:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,5 +8,4 @@ end
 def position_taken?(board, position)
   return false if [" ", "", nil].include?(board[position])
   return true if ["X", "O"].include?(board[position])
-  raise "#{board[position]} is not a valid move"
-end
+  end
